977f600f3a187e32651cbd6bf7c01226cdfd26f1
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-15 16:36:45 +0530

    update restful apis file

---
+++ SQLClass/index.js
@@ -0,0 +1,56 @@
+//index.js file is must to use any dependencies or write code in node.js. It is the entry point of the application. It is used to import the required modules and start the server.
+
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '1234'
+});
+ 
+
+//error method is used to handle the error in the connection. It is used to check if the connection is successful or not. If the connection is successful, it will return the result. If the connection is not successful, it will throw an error.
+//
+
+//let q = "SHOW TABLES";
+
+let getRandomUser =() => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+ 
+  ];
+};
+
+//inserting new data and placeholder is used to prevent SQL injection
+let q = "INSERT INTO users (id,username,email,password_hash) VALUES ?";
+//let users = ["123","abhishek","abhishekbillionare@truth.com","1234"];
+
+let data =[];
+for(let i=0;i<100;i++){
+    data.push(getRandomUser());//100 fake users will be generated and inserted into the database. you can change the number of users by changing the value of i in the for loop.
+}
+
+
+
+
+try{
+    connection.query(q,[data],(err,result)=>{
+    if (err) throw err;
+    console.log(result);
+    console.log(result.length);
+})
+
+}catch(err){
+    console.log(err);
+}
+
+//if we don't write connection.end() then the connection will remain open and it will cause memory leak. so it is a good practice to close the connection after the query is executed. it is used to close the connection to the database. It is used to free up the resources and avoid memory leak. It is a good practice to close the connection after the query is executed.
+connection.end();
+
+
---
+++ SQLClass/schema.sql
@@ -0,0 +1,8 @@
+-- in this file we will define the schema for our database
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);
