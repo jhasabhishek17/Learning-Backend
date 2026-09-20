1c462a25088322e3b4903c507e728e124c3b772c
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-20 23:32:41 +0530

    Added new section

---
+++ Mongo3/init.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+const chat = require("./models/chat.js");
+
+main().then(() => {
+    console.log("connection successful");
+ })
+
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+}
+
+
+
+
+
+let allChats = [
+{
+    from: "neha",
+    to: "priya",
+    msg: "send your exam sheets",
+    created_at: new Date(),
+},
+{
+    from: "rohit",
+    to: "neha",
+    msg: "send your",
+    created_at: new Date(),
+},
+{
+    from: "shubh",
+    to: "akshra",
+    msg: "exam sheets",
+    created_at: new Date(),
+},
+{
+    from: "akshra",
+    to: "shubh",
+    msg: "send sheets",
+    created_at: new Date(),
+},
+];
+
+Chat.insertMany(allChats);
