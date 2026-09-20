0c606d3aa65313334a36e48889009c4b624bb9e3
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-20 22:11:26 +0530

    mongo update

--- Mongo/index.js
+++ Mongo/index.js
@@ -12,3 +12,27 @@ async function main() {
 
 }
 
+//Below all the keys start with capital note that
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+// const User = mongoose.model("User", userSchema);
+
+// const user1 = new User({
+//   name: "Abhishek",
+//   email:"shubha@gmail.com",
+//   age: 22,
+// });
+
+// user1.save();
+
+User.find({})
+  .then((res) => {
+    console.log(err);
+})
+.catch((err) => {
+  console.log(err);
+});
